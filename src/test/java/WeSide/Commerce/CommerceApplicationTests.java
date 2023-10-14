1db16f0426960681f8845692e6d5826f42890b66
wattt3 <chleodnr3@gmail.com>
2023-10-14 13:39:43 +0900

    Init

---
+++ src/test/java/WeSide/Commerce/CommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package WeSide.Commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
