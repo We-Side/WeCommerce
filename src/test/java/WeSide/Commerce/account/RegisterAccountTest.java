949e8276788add2555f01638bdd1e39617656951
wattt3 <chleodnr3@gmail.com>
2023-10-31 15:55:46 +0900

    2. RegisterAccount 생성

--- src/test/java/WeSide/Commerce/account/RegisterAccountTest.java
+++ src/test/java/WeSide/Commerce/account/RegisterAccountTest.java
@@ -6,7 +6,18 @@ public class RegisterAccountTest {
 
     @Test
     void register() {
+        var sut = new RegisterAccount();
+
+        sut.register("email", "password", "username", "role", "phone", "address");
+
 
     }
 
+    public class RegisterAccount {
+
+        public void register(String email, String password, String username, String role,
+                String phone, String address) {
+
+        }
+    }
 }
