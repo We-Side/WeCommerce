b724f791e0ef197a4774cf64baf05b13bcbb2bca
wattt3 <chleodnr3@gmail.com>
2023-10-31 16:00:17 +0900

    3. Account 생성

--- src/test/java/WeSide/Commerce/account/RegisterAccountTest.java
+++ src/test/java/WeSide/Commerce/account/RegisterAccountTest.java
@@ -17,7 +17,27 @@ public class RegisterAccountTest {
 
         public void register(String email, String password, String username, String role,
                 String phone, String address) {
+            Account account = new Account(email, password, username, role, phone, address);
 
+        }
+    }
+
+    private class Account {
+
+        private String email;
+        private String password;
+        private String username;
+        private String role;
+        private String phone;
+        private String address;
+
+        public Account(String email, String password, String username, String role, String phone, String address) {
+            this.email = email;
+            this.password = password;
+            this.username = username;
+            this.role = role;
+            this.phone = phone;
+            this.address = address;
         }
     }
 }
