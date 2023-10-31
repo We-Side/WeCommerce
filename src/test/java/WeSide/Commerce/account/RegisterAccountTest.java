3ecb0cea881263777259f0df6a0daa300dabea0b
wattt3 <chleodnr3@gmail.com>
2023-10-31 16:09:31 +0900

    4. POJO AccountRepository 생성

--- src/test/java/WeSide/Commerce/account/RegisterAccountTest.java
+++ src/test/java/WeSide/Commerce/account/RegisterAccountTest.java
@@ -1,12 +1,16 @@
 package WeSide.Commerce.account;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 import org.junit.jupiter.api.Test;
 
 public class RegisterAccountTest {
 
     @Test
     void register() {
-        var sut = new RegisterAccount();
+        var accountRepository = new AccountRepository();
+        var sut = new RegisterAccount(accountRepository);
 
         sut.register("email", "password", "username", "role", "phone", "address");
 
@@ -15,15 +19,23 @@ public class RegisterAccountTest {
 
     public class RegisterAccount {
 
+        private final AccountRepository accountRepository;
+
+        public RegisterAccount(AccountRepository accountRepository) {
+            this.accountRepository = accountRepository;
+        }
+
         public void register(String email, String password, String username, String role,
                 String phone, String address) {
             Account account = new Account(email, password, username, role, phone, address);
 
+            accountRepository.save(account);
         }
     }
 
     private class Account {
 
+        private Long id;
         private String email;
         private String password;
         private String username;
@@ -38,6 +50,25 @@ public class RegisterAccountTest {
             this.role = role;
             this.phone = phone;
             this.address = address;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+    }
+
+    public class AccountRepository {
+
+        private final AtomicLong sequence = new AtomicLong(1L);
+        private final Map<Long, Account> accounts = new HashMap<>();
+
+        public void save(Account account) {
+            account.setId(sequence.getAndIncrement());
+            accounts.put(account.getId(), account);
         }
     }
 }
